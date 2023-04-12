5cedb293d04829d5bed6ca4f5dc32f3c335709ee
Greg-design <gregory_gr@outlook.com>
2023-04-12 11:53:33 -0300

    começando a criar a página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
   return (
     <>
-      <h1>Teste</h1>
+      <Head>
+        <title>Apaixonado por tecnologia</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>
+              Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que
+              usamos no mercado de trabalho. 👊
+            </span>
+
+            <a>
+              <button>Começar agora!</button>
+            </a>
+          </section>
+          <img src="/images/babyvictory.png" alt="Conteúdos Sujeito Programdor" />
+        </div>
+      </main>
     </>
   );
 }
