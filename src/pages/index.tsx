d3e0077f92a3b27f45ff322c56d7277d132f5191
Greg-design <gregory_gr@outlook.com>
2023-04-12 13:36:56 -0300

    terminando segunda parte da tela Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,30 @@ export default function Home() {
           </section>
           <img src="/images/babyvictory.png" alt="Conteúdos Sujeito Programdor" />
         </div>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda a criar aplicativos para Android e IOS</h2>
+            <span>
+              Você vai descobrir o jeito mais moderno de desenvolver apps nativos para IOS e Android, construindo
+              aplicativos do zero até aplicativos profissionais.
+            </span>
+          </section>
+          <img src="/images/demoApp.png" alt="Conteúdos sobre desenvolvimento de apps." />
+        </div>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.sectionContent}>
+          <img src="/images/demos1.png" alt="Conteúdos sobre desenvolvimento de aplicações web." />
+
+          <section>
+            <h2>Aprenda a criar sistemas web.</h2>
+            <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
+          </section>
+        </div>
       </main>
     </>
   );
